6c3daf6289a4a2e54e04bb63abec753e1f06fca7
azireert <bpis@hotmail.fr>
2019-06-24 13:45:40 +0200

    fix error

--- utilisateur/Utilisateur.js
+++ utilisateur/Utilisateur.js
@@ -3,7 +3,7 @@ var db = require('../db');
 var Utilisateur = {
     getUtilisateur: function(callback)
     {
-        return db.query('SELECT * from visiteur', callback);
+        return db.query('SELECT * from utilisateur', callback);
     },
 };
 
