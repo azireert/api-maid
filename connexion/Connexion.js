956bd38f5c145495ba0eebfc7d13b9c7ca7e823b
RealAdaas <ad.gavory@gmail.com>
2019-06-30 21:55:23 +0200

    Connexion

---
+++ connexion/Connexion.js
@@ -0,0 +1,18 @@
+var db = require('../db');
+
+var Connexion = {
+    
+    connect: function(email, mdp, callback)
+    {
+        if (db.query('SELECT * FROM maid WHERE email = ? AND mdp = ?', [email], [mdp]) != null)
+        {
+            return db.query('SELECT * FROM maid WHERE email = ? AND mdp = ?', [email], [mdp]);
+        }
+        if (db.query('SELECT * FROM utilisateur WHERE email = ? AND mdp = ?', [email], [mdp]))
+        {
+            return db.query('SELECT * FROM utilisateur WHERE email = ? AND mdp = ?', [email], [mdp]);
+        } 
+    }
+}
+
+module.exports = Connexion;
